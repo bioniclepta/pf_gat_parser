1e7e516d52659c76ac737575909751f1a577610e
bioniclepta <85424643+bioniclepta@users.noreply.github.com>
2025-08-27 16:17:54 -0400

    Fixed incorrect indexing
    The index was 2 higher than it needed to be

--- src/io/psse/components/system_switching_device.rs
+++ src/io/psse/components/system_switching_device.rs
@@ -127,29 +127,28 @@ pub fn parse_system_switching_device(lines: &[&[u8]]) -> Vec<SystemSwitchingDevi
     lines.par_iter().filter_map(|line_bytes| {
         from_utf8(line_bytes).ok().and_then(|line| {
             let parts: Vec<&str> = line.split(",").map(|s| s.trim()).collect();
-            println!("{:?}", parts);
             Some(SystemSwitchingDevice{
                 from_bus: parts[0].parse().unwrap_or(0),
                 to_bus: parts[1].parse().unwrap_or(0),
                 circuit: parts[2].replace("'", "").trim().to_string(),
                 x: parts[3].parse().unwrap_or(0.0001),
-                rate1: parts[6].parse().unwrap_or(0.0),
-                rate2: parts[7].parse().unwrap_or(0.0),
-                rate3: parts[8].parse().unwrap_or(0.0),
-                rate4: parts[9].parse().unwrap_or(0.0),
-                rate5: parts[10].parse().unwrap_or(0.0),
-                rate6: parts[11].parse().unwrap_or(0.0),
-                rate7: parts[12].parse().unwrap_or(0.0),
-                rate8: parts[13].parse().unwrap_or(0.0),
-                rate9: parts[14].parse().unwrap_or(0.0),
-                rate10: parts[15].parse().unwrap_or(0.0),
-                rate11: parts[16].parse().unwrap_or(0.0),
-                rate12: parts[17].parse().unwrap_or(0.0),
-                status: parts[18].parse().unwrap_or(1),
-                normal_status: parts[19].parse().unwrap_or(1),
-                meter_end: parts[20].parse().unwrap_or(1),
-                device_type: parts[21].parse().unwrap_or(1),
-                name: parts[22].replace("'", "").trim().to_string(),
+                rate1: parts[4].parse().unwrap_or(0.0),
+                rate2: parts[5].parse().unwrap_or(0.0),
+                rate3: parts[6].parse().unwrap_or(0.0),
+                rate4: parts[7].parse().unwrap_or(0.0),
+                rate5: parts[8].parse().unwrap_or(0.0),
+                rate6: parts[9].parse().unwrap_or(0.0),
+                rate7: parts[10].parse().unwrap_or(0.0),
+                rate8: parts[11].parse().unwrap_or(0.0),
+                rate9: parts[12].parse().unwrap_or(0.0),
+                rate10: parts[13].parse().unwrap_or(0.0),
+                rate11: parts[14].parse().unwrap_or(0.0),
+                rate12: parts[15].parse().unwrap_or(0.0),
+                status: parts[16].parse().unwrap_or(1),
+                normal_status: parts[17].parse().unwrap_or(1),
+                meter_end: parts[18].parse().unwrap_or(1),
+                device_type: parts[19].parse().unwrap_or(1),
+                name: parts[20].replace("'", "").trim().to_string(),
             })
         })
     }).collect()
